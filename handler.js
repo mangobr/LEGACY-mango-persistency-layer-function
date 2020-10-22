13f8ddd0509dc6d011eef25d5c959902d816c0f8
Christian <christian.trindade94@gmail.com>
2020-10-22 11:34:29 -0300

    modify handle to test

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.mangoDBOperations = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: "foi agora",
+      message: "teste 22-10",
       input: event,
     }),
   };
