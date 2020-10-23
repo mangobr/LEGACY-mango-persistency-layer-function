6f937c1ee169f8664f3ccf76c9dd211d041a83e9
Jeremias <moraesjeremias@Jeremiass-MacBook-Pro.local>
2020-10-23 02:17:52 -0300

    Adding operations test

---
+++ tests/operations.test.js
@@ -0,0 +1,19 @@
+const { describe, it } = require("@jest/globals");
+const { verifyGeneralFoodMismatch } = require("../operations");
+const {
+  returnsFoodLabel,
+  returnsFoodCandidateOnMatch,
+  returnsFoodCandidateOnMismatch,
+} = require("./__mocks__/mockkOperations.js");
+
+describe(`should test operations and tranformations prior database transactions`, () => {
+  it(`should validate two equal foods on match`, () => {
+      const verifiedLabel = verifyGeneralFoodMismatch(returnsFoodLabel(), returnsFoodCandidateOnMatch())
+      expect(verifiedLabel).toEqual(returnsFoodCandidateOnMatch())
+  });
+
+  it(`should not validate two different foods on match`, () => {
+    const verifiedLabel = verifyGeneralFoodMismatch(returnsFoodLabel(), returnsFoodCandidateOnMismatch())
+    expect(verifiedLabel).not.toEqual(returnsFoodCandidateOnMismatch())
+});
+});
