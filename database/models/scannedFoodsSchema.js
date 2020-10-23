754adda1fb992fcbd3b44b49ca39e1fef69259e4
Jeremias <moraesjeremias@Jeremiass-MacBook-Pro.local>
2020-10-23 00:55:05 -0300

    addding updateDate e s3Url to scannedFood schema

--- database/models/scannedFoodsSchema.js
+++ database/models/scannedFoodsSchema.js
@@ -5,6 +5,8 @@ const ScannedFoods = new mongoose.Schema({
     userId: String,
     consolidatedScannedFood: [Object],
     createDate: String,
+    updateDate: Date,
+    s3URL: String
 })
 
 module.exports = mongoose.model('ScannedFoods', ScannedFoods, 'scanned-foods-collection')
