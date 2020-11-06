8c0824b4271e89a139fa5a87b85625cacc871dce
Jeremias <moraesjeremias@Jeremiass-MacBook-Pro.local>
2020-11-06 19:57:02 -0300

    removing unused operations

--- operations.js
+++ operations.js
@@ -1,27 +1,8 @@
-const {
-  findTacoFoodDescriptionByName,
-  insertValidatedFoodAssignment,
-} = require("./mongoTransactions");
-
-const someFx = (labelList) => {
-  labelList.map(async (label) => {
-    const tacoResponse = await findTacoFoodDescriptionByName(label.Name);
-    if (tacoResponse.length > 0) {
-      tacoResponse.map((foodCandidate, index) => {
-        const verifiedFoodLabel = verifyGeneralFoodMismatch(
-          label.Name,
-          foodCandidate
-        );
-      });
-    }
-  });
-};
-
 const verifyGeneralFoodMismatch = (foodLabel, foodCandidate) => {
   const foodCandidateFirstWord = foodCandidate.description.split(",")[0];
   if (foodCandidateFirstWord === foodLabel) return foodCandidate;
 };
 
 module.exports = {
-    verifyGeneralFoodMismatch
+    verifyGeneralFoodMismatch,
 }
